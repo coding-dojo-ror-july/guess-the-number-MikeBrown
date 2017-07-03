25ff07ceaabe43ff721fb0da9b98aa27be54abc9
Mike Brown <mikebrownn@hotmail.com>
2017-07-03 19:24:44 +0000

    first commit

---
+++ guess_the_number.rb
@@ -0,0 +1,11 @@
+def guess_number guess 
+    number = 25
+    if guess == number
+        puts 'You got it!'
+    elsif guess < number
+        puts 'Guess was too low'
+    elsif guess > number
+        puts 'Guess was too high'
+    end
+end
+guess_number 28
